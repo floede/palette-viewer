9392a45244fd6e65d07a1b5187ed0b38751b8672
Janus Hasseriis <janus@hasseriis.net>
2022-12-22 23:48:54 +0100

    Update color to new format

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log(colors);
 
-const palettes = `<span>TEST</span>${colors
-  .map(
-    (palette, index) => `<h2>${paletteNames[index]}</h2>
+let palettes;
+
+if (colors.paletteNames) {
+  palettes = `<span>TEST</span>${colors
+    .map(
+      (palette, index) => `<h2>${paletteNames[index]}</h2>
       <div class="palette">${palette
         .map(
           (color, index) => `
@@ -13,7 +16,23 @@ const palettes = `<span>TEST</span>${colors
           `
         )
         .join("")}</div>`
-  )
-  .join("")}`;
-
+    )
+    .join("")}`;
+} else {
+  palettes = `<span>TEST</span>${colors
+    .map(
+      (palette, index) => `<h2>${palette.name}</h2>
+        <div class="palette">${palette.colArr
+          .map(
+            (color, index) => `
+              <div class="infoBox">  
+                <div class="color" style="background: ${color.hex};"></div>  
+                <div>${color.name}</div>
+              </div>
+            `
+          )
+          .join("")}</div>`
+    )
+    .join("")}`;
+}
 document.getElementById("palettes").innerHTML = palettes;
